34423094a8880d9111e6a25174fa45e7918bae33
rblaise <rblaise@kaplan.edu>
2014-03-26 12:46:06 -0400

    Added UserName to Student Object

--- GitTestSolution/Student.cs
+++ GitTestSolution/Student.cs
@@ -11,5 +11,7 @@ namespace GitTestSolution
         public int StudentID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public string Username { get; set; }
     }
 }
